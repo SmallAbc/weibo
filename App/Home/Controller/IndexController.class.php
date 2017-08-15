16737255edbe3fe9312a1c2288d55be15485c4f3
Austine Wang <497643743@qq.com>
2017-08-15 14:49:40 +0800

    4.21 微博主页设计(上)

--- App/Home/Controller/IndexController.class.php
+++ App/Home/Controller/IndexController.class.php
@@ -1,14 +1,10 @@
 <?php
 namespace Home\Controller;
 
-use function dump;
-use function session;
-
 class IndexController extends HomeController{
     public function index(){
         if($this->login()){
-            echo '登录成功!';
-            dump(session());
+            $this->display();
         }
     }
 }
